b463410b6e96e737c849f36f163b74e843ff4f04
mathisT <mathis.toinon@etu.univ-lyon1.fr>
2022-06-05 12:29:14 +0200

    interface screen2

--- code/src/IHM/Screen2.java
+++ code/src/IHM/Screen2.java
@@ -1,9 +1,14 @@
 package IHM;
 
+import NodePackage.Flag;
 import NodePackage.ListCity;
+import NodePackage.Neighbor;
+import NodePackage.Place;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Screen2 extends JPanel {
     private ListCity listCity;
@@ -35,5 +40,51 @@ public class Screen2 extends JPanel {
         this.setLayout(new BorderLayout());
         this.add(left, BorderLayout.WEST);
         this.add(graph, BorderLayout.CENTER);
+
+
+        JLabel cityChoice1 = new JLabel("Choissisez une première ville");
+        JComboBox<String> cityCombo1 = new JComboBox<>(listCity.getListName());
+        cityCombo1.setMaximumSize(new Dimension(200, 40));
+
+        JLabel cityChoice2 = new JLabel("Choissisez une seconde ville");
+        JComboBox<String> cityCombo2 = new JComboBox<>(listCity.getListName());
+        cityCombo2.setMaximumSize(new Dimension(200, 40));
+
+        JButton check = new JButton("Valider");
+        check.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Place city;
+                int numberOf2neighbor = 0;
+                String city1 = cityChoice1.getText();
+                if ((city = (Place) listCity.findByName(city1)) != null) {
+                    Neighbor tmp = city.getHead();
+                    Neighbor tmp2;
+                    while (tmp != null) {
+                        tmp2 = listCity.findByName(tmp.getName()).getHead();
+                        while(tmp2 != null){
+                            if(tmp2.getName().equals(cityChoice2.getText())){
+
+                            }
+                            tmp2 = (Neighbor) tmp2.next;
+                        }
+
+                        tmp = (Neighbor) tmp.next;
+                    }
+                }
+            }
+        });
+
+        JLabel answer = new JLabel("");
+
+        left.add(cityChoice1);
+        left.add(cityCombo1);
+        left.add(cityChoice2);
+        left.add(cityCombo2);
+        left.add(answer);
+        cityChoice1.setAlignmentX(CENTER_ALIGNMENT);
+        cityChoice2.setAlignmentX(CENTER_ALIGNMENT);
+        cityCombo1.setAlignmentX(CENTER_ALIGNMENT);
+        cityCombo1.setAlignmentX(CENTER_ALIGNMENT);
     }
 }
