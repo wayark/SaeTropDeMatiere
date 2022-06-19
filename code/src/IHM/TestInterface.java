7ee792fc420e985d79a8dd2406c4eabbbd035870
liam <liam.lucas693@gmail.com>
2022-06-19 21:39:12 +0200

    Finalisation SAE

--- code/src/IHM/TestInterface.java
+++ code/src/IHM/TestInterface.java
@@ -6,11 +6,20 @@ import java.io.IOException;
 import NodePackage.ListCity;
 import Reader.CsvReader;
 
+import javax.swing.*;
+
 public class TestInterface {
     public static void main(String[] args) throws IOException {
 
         ListCity l = new ListCity();
         CsvReader c = new CsvReader(l);
-        MainWindow mainWindow = new MainWindow(l);
+        JFrame frame = new JFrame();
+        frame.setSize(1500,800);
+        //Screen0 s = new Screen0(l,frame);
+        //Screen1 s = new Screen1(l,frame);
+        //Screen2 s = new Screen2(l,frame);
+        Screen3 s = new Screen3(l,frame);
+        frame.setContentPane(s);
+        frame.setVisible(true);
     }
 }
--- code/src/Main.java
+++ code/src/Main.java
@@ -1,3 +1,4 @@
+import IHM.MainWindow;
 import NodePackage.ListCity;
 import Reader.CsvReader;
 
@@ -8,28 +9,7 @@ public class Main {
 
         ListCity l = new ListCity();
         CsvReader c = new CsvReader(l);
-
-        l.shortestroute(l.findByName("Lyon"),l.findByName("Bellecour"));
-
-      /*  //Recherche selon nom
-       listCity.findByName("Lyon");
-
-
-        //Afficher tout les lieux
-        listCity.showByType("V");
-        System.out.println("------------");
-
-        //Lister tout les lieux
-        listCity.showList();
-        System.out.println("------------");
-
-        //affichage sommet voisin
-        listCity.showNeighbor("Lyon");
-        System.out.println("------------");
-
-        //affichage sommet voisin distance 2
-        listCity.showTwoNeighbor("Lyon");
-        System.out.println("------------");*/
+        MainWindow mainWindow = new MainWindow(l);
 
     }
 }
